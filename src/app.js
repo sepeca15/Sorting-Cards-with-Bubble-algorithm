bbbe5768e963beb1b96fa4f81555f0ce6aaa8208
sepeca15 <sepeca15@gmail.com>
2021-04-22 20:36:37 +0000

    Cartas ok sin JS

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let drow = document.querySelector("#drow");
+drow.addEventListener("click", function(e) {
+  e.preventDefault();
+  let entCard = document.querySelector("#entCard");
+  var cartasAgenerar = [];
+  cartasAgenerar.push(entCard.value);
+  console.log(cartasAgenerar);
+  drow.disabled = true;
+
+  /*   for(){
+  } */
+});
